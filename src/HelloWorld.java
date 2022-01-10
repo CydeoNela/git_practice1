00da2b92168d97f3b5c029ed417be2a0608220b6
Daniela <nelavip@yahoo.com>
2022-01-10 14:44:47 -0800

    work 3 is done and about to be pushed to Github commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
 public class HelloWorld {
     //doing some work here(you can type anything you want)
     //doing awesome work 2!
+    //doing awesome work 3!
+    //Awesome work is done and ready to be comminted
+    //and ready to be pushed to remote repository
 }
